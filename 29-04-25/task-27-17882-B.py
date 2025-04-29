fcea62e7533fff055c2ce0b33d0f080164df67c3
DHudonogov <dhudonogov2001@gmail.com>
2025-04-29 19:53:56 +0200

    task-27

--- 29-04-25/task-27-17882-B.py
+++ 29-04-25/task-27-17882-B.py
@@ -15,17 +15,18 @@ with open('27_B_17882.txt') as file:
     cluster3 = []
     for i in file:
         x, y = map(float, i.split())
-        if 0 < x < 4:
+        if 0 < y < 4:
             cluster1.append([x, y])
-        if 4 < x < 7:
+        if 4 < y < 7:
             cluster2.append([x, y])
-        else:
+        if y > 7:
             cluster3.append([x, y])
 
 center1 = centroid(cluster1)
 center2 = centroid(cluster2)
+center3 = centroid(cluster3)
 
-ans_x = (center1[0] + center2[0]) / 2
-ans_y = (center1[1] + center2[1]) / 2
+ans_x = (center1[0] + center2[0] + center3[0]) / 3
+ans_y = (center1[1] + center2[1] + center3[1]) / 3
 
 print(int(ans_x * 10000), int(ans_y * 10000))
